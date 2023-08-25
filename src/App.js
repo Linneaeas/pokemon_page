2e05353e8c61f1a916cdb0e82cf8b1b86c9ae36b
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-08-25 11:34:37 +0200

    Got axios for fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import PokemonList from "./PokemonList";
 
 function App() {
-  return <div className="App"></div>;
+  const [pokemon, setPokemon] = useState(["bulbasaur", "charmander"]);
+
+  useEffect(() => {}, []);
+
+  axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
+    setPokemon(res.data.results.map((p) => p.name));
+  });
+
+  return <PokemonList pokemon={pokemon} />;
 }
 
 export default App;
