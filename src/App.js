3999f20f59ec6d9ff7b812e2783a5f54b974bed2
Linnea <127832519+Linneaeas@users.noreply.github.com>
2023-08-25 12:04:04 +0200

    loading message

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import PokemonList from "./PokemonList";
+import axios from "axios";
 
 function App() {
-  const [pokemon, setPokemon] = useState(["bulbasaur", "charmander"]);
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [prevPageUrl, setPrevPageUrl] = useState();
+  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    setLoading(true);
+    axios.get(currentPageUrl).then((res) => {
+      setLoading(false);
+      setNextPageUrl(res.data.next);
+      setPrevPageUrl(res.data.previous);
+      setPokemon(res.data.results.map((p) => p.name));
+    });
+  }, [currentPageUrl]);
 
-  axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
-    setPokemon(res.data.results.map((p) => p.name));
-  });
+  if (loading) return "Loading...";
 
   return <PokemonList pokemon={pokemon} />;
 }
